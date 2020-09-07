81fbfab0fceee1287c2a1cb68ec00d1cf364659f
Max Anderberg <maxanderberg@gmail.com>
2020-09-07 17:07:12 +0200

    Added a flipsprite method. If player's velocity is - (moving left) then flip sprite

---
+++ Vania/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+    Rigidbody2D myRigidbody;
+    SpriteRenderer mySpriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Run();
+        FlipSprite();
+
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // Value is between -1 & +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+        
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            Debug.Log("Hello");
+        }
+    }
+
+    private void FlipSprite()
+    {
+        
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    
+
+ 
+
+
+
+    }
+}
