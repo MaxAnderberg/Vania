1bb72a0f8154e3c623c529372587555645213b2f
Max Anderberg <maxanderberg@gmail.com>
2020-09-12 21:54:00 +0200

    init

---
+++ Vania/Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+
+    int currentSceneIndex;
+    int startingSceneIndex;
+
+    private void Awake()
+    {
+        
+        int numberOfScenePersist = FindObjectsOfType<ScenePersist>().Length;
+        if (numberOfScenePersist > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != startingSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
